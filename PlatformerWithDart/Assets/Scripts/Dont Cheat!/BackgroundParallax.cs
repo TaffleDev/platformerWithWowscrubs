ecd1c4565dfcffa10a61229fa83e262bb7e88a5d
Seidelin2 <casperseidelin2@gmail.com>
2024-08-17 17:58:40 +0200

    updated levle added testR

--- PlatformerWithDart/Assets/Scripts/Dont Cheat!/BackgroundParallax.cs
+++ PlatformerWithDart/Assets/Scripts/Dont Cheat!/BackgroundParallax.cs
@@ -4,7 +4,8 @@ public class BackgroundParallax : MonoBehaviour
 {
 
     private float length, startpos;
-    public GameObject cam;
+    [SerializeField]
+    private GameObject cam;
     public float parallaxEffect;
 
     void Start()
